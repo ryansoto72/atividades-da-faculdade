5183f384e8498c4bd7d64a01ba1831cdefbf5da1
Ryan Soto <ryansoto72@gmail.com>
2023-06-14 03:42:51 -0300

    ex sala

---
+++ list_exer/lista3/ex05.py
@@ -0,0 +1,24 @@
+while True:
+
+    c1 = input("Digite o codico do produto:")
+
+    if c1 == "100":
+        print("O produto selecionado foi o (CACHORRO QUENTE) custa (3,50)")
+        break
+    elif c1 == "101":
+        print("O produto selecionado foi o (BAURU SIMPLES) custa (3,80)")
+        break
+    elif c1 == "102":
+        print("O produto selecionado foi o (BAURU COM OVO) custa (4,50)")
+        break
+    elif c1 == "103":
+        print("O produto selecionado foi o (HAMBURGUER) custa (4,70)")
+        break
+    elif c1 == "104":
+        print("O produto selecionado foi o (CHEESEBURGER) custa (3,50)")
+        break
+    elif c1 == "105":
+        print("O produto selecionado foi o (REFRIGERANTE) custa (4,00)")
+        break
+    else:
+        print("Digite um codico valido ")
